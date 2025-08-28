d1eb6ceecb4a7294b964fdd2af1850b63779c686
Luis Neumann <luisgneumannc@hotmail.com>
2025-08-28 15:58:44 -0600

    Upload to repository. React micro front end to display the airbnb clone react property grid.

---
+++ src/PropertyApp.tsx
@@ -0,0 +1,21 @@
+import './PropertyApp.css'
+import { PropertyCard } from './ui/PropertyCard'
+import type { Property } from './model/types'
+
+const PropertyApp = ({ properties }: { properties: Property[] }) => {
+    return (
+        <div className="property-grid-container">
+            <h1>Popular Destinations</h1>
+            <div className="property-grid">
+                {properties.map((property: Property) => (
+                    <PropertyCard
+                        key={property.id}
+                        {...property}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default PropertyApp
