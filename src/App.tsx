258e4a92cfc5b0a699fe75ff7c0532ab10153101
Luis Neumann <luisgneumannc@hotmail.com>
2025-09-01 13:11:09 -0600

    Added playwright testing framework configuration and tests. Luis Neumann. September 1st., 2025.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PropertyApp from "./PropertyApp";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const properties = [
+        {
+            id: 1,
+            image: "https://example.com/image.jpg",
+            title: "Property Title",
+            type: "Property Type",
+            location: "Property Location",
+            details: "Property Details",
+            host: "Property Host",
+            price: 100,
+            rating: 5,
+        }
+    ]
+
+    return (
+        <>
+            <p>Property App</p>
+            <PropertyApp properties={properties} />
+        </>
+    )
 }
 
 export default App
